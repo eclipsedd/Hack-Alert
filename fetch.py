32c884365cc4475212bc8f1fd54a6c9466303755
Aryan Choudhary <aryan05ch@gmail.com>
2024-12-10 21:50:53 +0530

    init: fetch function

---
+++ fetch.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def loader(url, path):
+    r = requests.get(url)
+    with open(path, "w", encoding="utf-8") as f:
+        f.write(r.text)
+
+
+url = "https://www.hackerearth.com/challenges/?filters=competitive%2Chackathon%2Cuniversity"
+path = "data.html"
+
+loader(url, path)
